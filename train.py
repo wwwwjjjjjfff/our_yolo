399a5cc1a8df7d10dc61d42e17099c6fbc6097d0
wwwwjjjjjfff <156509291+wwwwjjjjjfff@users.noreply.github.com>
2025-08-09 17:42:51 +0800

    Update train.py

--- train.py
+++ train.py
@@ -5,7 +5,7 @@ from ultralytics import YOLO
 
 if __name__ == '__main__':
     model = YOLO(model=r"../ultralytics/cfg/models/11/DFF_MSCA.yaml")
-    model.train(data="../mydata/danwan8_4.yaml", epochs=100, imgsz=[640, 640], batch=4, lr0=0.01,classes=0,)
+    model.train(data="../mydata/danwan8_4.yaml", epochs=100, imgsz=[640, 640], batch=2, lr0=0.01,classes=0,)
 
 
 
